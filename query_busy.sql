6829edf8677d27e7c13357301cc4edbd5a9224d0
onion0sugar <kacperbukala@gmail.com>
2026-08-25 15:07:24 +0200

    2

--- query_busy.sql
+++ query_busy.sql
@@ -4,7 +4,8 @@ FROM [SerwisKop_Magazyn].[Document].[Documents] DD
 LEFT JOIN Core.Users CU ON CU.Id = TRY_CONVERT(int, DD.ModifiedBy)
 LEFT JOIN [SerwisKop_Magazyn].[Document].[CustomerOrderDocumentConfigurations] CONF
   ON CONF.Id = DD.CustomerOrderDocumentConfigurationId
+LEFT JOIN Package.PackageStats PS ON PS.DocumentId = DD.Id
 WHERE DD.DateCreatedUtc >= DATEADD(DAY, -14, GETUTCDATE())
   AND DD.SubType = 50
   AND (DD.DocumentType = 7 AND DD.DocumentStatusText = 'in_progress')
-  OR (DD.DocumentType = 22 AND CONF.CourierId = 13)
+  OR (DD.DocumentType = 22 AND CONF.CourierId = 13 AND DD.DocumentStatusText = 'in_progress' AND PS.SuccessScan = 0)
