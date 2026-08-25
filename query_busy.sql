5acf66066d7af5971638ef777feee8078db4a08c
onion0sugar <kacperbukala@gmail.com>
2026-08-25 15:22:57 +0200

    2

--- query_busy.sql
+++ query_busy.sql
@@ -8,4 +8,4 @@ LEFT JOIN Package.PackageStats PS ON PS.DocumentId = DD.Id
 WHERE DD.DateCreatedUtc >= DATEADD(DAY, -14, GETUTCDATE())
   AND DD.SubType = 50
   AND (DD.DocumentType = 7 AND DD.DocumentStatusText = 'in_progress')
-  OR (DD.DocumentType = 22 AND CONF.CourierId = 13 AND DD.DocumentStatusText = 'in_progress' AND PS.SuccessScan != 0)
+  --OR (DD.DocumentType = 22 AND CONF.CourierId = 13 AND DD.DocumentStatusText != 'end')
