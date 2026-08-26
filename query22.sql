c90085a0358ab03114e32a68c65bf64deb568ba3
onion0sugar <kacperbukala@gmail.com>
2026-08-26 13:16:48 +0200

    in_progress/new

--- query22.sql
+++ query22.sql
@@ -10,7 +10,8 @@ FROM [SerwisKop_Magazyn].[Document].[Documents] DD
 LEFT JOIN [SerwisKop_Magazyn].[Document].[CustomerOrderDocumentConfigurations] CONF
   ON CONF.Id = DD.CustomerOrderDocumentConfigurationId
 LEFT JOIN Core.Users CU ON CU.Id = TRY_CONVERT(int, DD.ModifiedBy)
+LEFT JOIN [SerwisKop_Magazyn].[Package].[PackagePositions] ppp ON PPP.DocumentId = DD.ID
 WHERE DD.DateCreatedUtc >= DATEADD(DAY, -30, GETUTCDATE())
   AND DD.SubType = 50
-  AND ((DD.DocumentType = 22 AND DD.DocumentStatusText IN ('in_progress', 'new') AND CONF.CourierId = 13)
+  AND ((DD.DocumentType = 22 AND DD.DocumentStatusText = 'new')
   OR (DD.DocumentType = 7 AND DD.DocumentStatusText = 'new'))
