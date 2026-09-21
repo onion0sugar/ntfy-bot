0086ca8e8b06bbbb67577ba69a249e3f44110be6
onion0sugar <kacperbukala@gmail.com>
2026-09-21 14:34:40 +0200

    porawa working

--- work_today_users.sql
+++ work_today_users.sql
@@ -1,10 +1,10 @@
 -- Users from users.txt who modified at least one document today.
 -- {usernames} is replaced with parameter placeholders by the application.
 SELECT CU.UserName,
-       MAX(ZGU.ZoneGroupId) AS MaxZoneGroupId
-FROM [SerwisKop_Magazyn].[Document].[Documents] DD
-INNER JOIN Core.Users CU ON DD.ModifiedBy = CU.Id
-LEFT JOIN [SerwisKop_Magazyn].[Zone].[ZoneGroupUsers] ZGU ON CU.Id = ZGU.UserId
-WHERE DD.DateModifiedUtc >= CAST(GETDATE() AS DATE)
+MAX(ZGU.ZoneGroupId) AS MaxZoneGroupId
+  FROM [SerwisKop_Magazyn].[Package].[PackagePositions] DD
+  INNER JOIN Core.Users CU ON DD.CreatedBy = CU.Id
+  LEFT JOIN [SerwisKop_Magazyn].[Zone].[ZoneGroupUsers] ZGU ON CU.Id = ZGU.UserId
+    WHERE DD.DateModifiedUtc >= CAST(GETDATE() AS DATE)
   AND CU.UserName IN ({usernames})
 GROUP BY CU.UserName;
